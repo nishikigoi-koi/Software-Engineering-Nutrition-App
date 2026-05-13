200921515a36d7ad6425ce8a4ba5aea5b66fc47c
Nishi <41277001+nishikigoi-koi@users.noreply.github.com>
2026-05-13 14:54:20 +1200

    Sign up page design
    Design for sign up page, comments for clarity on main.dart.

--- Flutter/flutter_app_web/lib/main.dart
+++ Flutter/flutter_app_web/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login 2025',
+      title: 'Nutrition App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -18,6 +18,8 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// -- Login Page --
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -101,12 +103,17 @@ class _LoginPageState extends State<LoginPage> {
                     showDialog(
                       context: context,
                       builder: (context) {
-                        if (_usernameController.text.trim().isEmpty || _passwordController.text.trim().isEmpty) {
+
+                        // Check if username or password are empty
+                        if (_usernameController.text.trim().isEmpty ||
+                            _passwordController.text.trim().isEmpty) {
                           return AlertDialog(
                             content: Text("Please make sure you have filled both fields."),
                           );
                         }
 
+                        // Print details for now.
+                        // TODO: Connect to database, compare against existing credentials. | LOGIN
                         return AlertDialog(
                           content: Text("Username: ${_usernameController.text}\nPassword: ${_passwordController.text}"),
                         );
@@ -133,7 +140,15 @@ class _LoginPageState extends State<LoginPage> {
               ),
 
               SizedBox(height: 10),
-              Center(
+
+              // Clickable text for viewing Sign In page
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                  );
+                },
                 child: Text(
                   "Don't have an account? Sign Up",
                   textAlign: TextAlign.center,
@@ -144,6 +159,142 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
               )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// -- Sign up Page --
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+ 
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+ 
+class _SignUpPageState extends State<SignUpPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+ 
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+ 
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Create Account',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26,
+                  color: Color(0xFF1C1C1C),
+                ),
+              ),
+ 
+              SizedBox(height: 6),
+              Text(
+                'Sign Up to get started',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.normal,
+                  fontSize: 18,
+                  color: Color(0xFF1C1C1C),
+                ),
+              ),
+ 
+              SizedBox(width: size.width / 4, height: 26),
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+ 
+              SizedBox(height: 26),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+ 
+              SizedBox(height: 26),
+              SizedBox(
+                width: size.width / 4,
+                height: 49,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Check if username or password are empty
+                    if (_usernameController.text.trim().isEmpty ||
+                        _passwordController.text.trim().isEmpty) {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          content: Text(
+                              "Please make sure you have filled both fields."),
+                        ),
+                      );
+                      return;
+                    }
+ 
+                    // Return to Login page for now.
+                    // TODO: Connect to database, compare against existing credentials. | SIGN UP
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF3B62FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+ 
+              SizedBox(height: 10),
+
+              // Clickable link to return to Login page
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  "Already have an account? Sign In",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    color: Color(0xFF87879D),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
