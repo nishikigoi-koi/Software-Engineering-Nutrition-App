935fae0c4da202e3b0122fec154b5453a0a012cd
Nishi <41277001+nishikigoi-koi@users.noreply.github.com>
2026-05-19 18:52:33 +1200

    Bug fix: input boxes resize with window

--- Flutter/flutter_app_web/lib/pages/login_page.dart
+++ Flutter/flutter_app_web/lib/pages/login_page.dart
@@ -21,125 +21,128 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-
-    Size size = MediaQuery.of(context).size;
-    
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Welcome',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 26,
-                  color: Color(0xFF1C1C1C),
-                ),
-              ),
-
-              SizedBox(height: 6),
-              Text(
-                'Sign In to continue',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.normal,
-                  fontSize: 18,
-                  color: Color(0xFF1C1C1C),
-                ),
-              ),
-
-              SizedBox(width: size.width / 4, height: 26),
-
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              SizedBox(height: 26),
-              TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true, // hide password 
-              ),
-              
-              SizedBox(height: 26),
-
-              SizedBox(
-                width: size.width / 4,
-                height: 49,
-                child: ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-
-                        // Check if username or password are empty
-                        if (_usernameController.text.trim().isEmpty ||
-                            _passwordController.text.trim().isEmpty) {
-                          return AlertDialog(
-                            content: Text("Please make sure you have filled both fields."),
-                          );
-                        }
-
-                        // Print details for now.
-                        // TODO: Connect to database, compare against existing credentials. | LOGIN
-                        return AlertDialog(
-                          content: Text("Username: ${_usernameController.text}\nPassword: ${_passwordController.text}"),
-                        );
-                      },
-                    );
-                  },
-
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF3B62FF),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 10),
-
-              // Clickable text for viewing Sign In page
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpPage()),
-                  );
-                },
-                child: Text(
-                  "Don't have an account? Sign Up",
-                  textAlign: TextAlign.center,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 400),
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome',
                   style: TextStyle(
                     fontFamily: 'Poppins',
-                    fontSize: 14,
-                    color: Color(0xFF87879D),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 26,
+                    color: Color(0xFF1C1C1C),
                   ),
                 ),
-              )
-            ],
+
+                SizedBox(height: 6),
+                Text(
+                  'Sign In to continue',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.normal,
+                    fontSize: 18,
+                    color: Color(0xFF1C1C1C),
+                  ),
+                ),
+
+                SizedBox(height: 26),
+
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+
+                SizedBox(height: 26),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true, // hide password
+                ),
+
+                SizedBox(height: 26),
+
+                SizedBox(
+                  width: 400,
+                  height: 49,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          // Check if username or password are empty
+                          if (_usernameController.text.trim().isEmpty ||
+                              _passwordController.text.trim().isEmpty) {
+                            return AlertDialog(
+                              content: Text(
+                                "Please make sure you have filled both fields.",
+                              ),
+                            );
+                          }
+
+                          // Print details for now.
+                          // TODO: Connect to database, compare against existing credentials. | LOGIN
+                          return AlertDialog(
+                            content: Text(
+                              "Username: ${_usernameController.text}\nPassword: ${_passwordController.text}",
+                            ),
+                          );
+                        },
+                      );
+                    },
+
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF3B62FF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                // Clickable text for viewing Sign In page
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpPage()),
+                    );
+                  },
+                  child: Text(
+                    "Don't have an account? Sign Up",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      color: Color(0xFF87879D),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
