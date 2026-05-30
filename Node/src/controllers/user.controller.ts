d90fc67e24543c4f58bd445989a6df95af3f04d9
Ostim12 <ostim12121212@gmail.com>
2026-05-30 18:47:09 +1200

    JWToken generation, and auth middleware made and set on routes

--- Node/src/controllers/user.controller.ts
+++ Node/src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import Bcrypt from 'bcrypt';
-
+import jwt from 'jsonwebtoken';
 
 export async function hashPassword(password: string): Promise<string> {
     const salt = await Bcrypt.genSalt(10);
@@ -8,4 +8,11 @@ export async function hashPassword(password: string): Promise<string> {
 
 export async function comparePassword(password: string, hash: string): Promise<boolean> {
     return await Bcrypt.compare(password, hash);
+}
+
+export function generateToken(userId: string): string {
+    const secret = process.env.JWT_SECRET as string;
+    const envExpiresIn = process.env.JWT_EXPIRES_IN as unknown as jwt.SignOptions['expiresIn'];
+    const options: jwt.SignOptions = { expiresIn: envExpiresIn };
+    return jwt.sign({ userId }, secret, options);
 }
