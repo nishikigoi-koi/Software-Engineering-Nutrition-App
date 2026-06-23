e813e6bfe1b06936d2ba9682991d7a7dd68f0449
Nishi <41277001+nishikigoi-koi@users.noreply.github.com>
2026-06-23 20:27:06 +1200

    Bug fix - updateCustomFood sent carbohydrate instead of carbohydrates

--- Flutter/flutter_app_web/lib/services/custom_food_service.dart
+++ Flutter/flutter_app_web/lib/services/custom_food_service.dart
@@ -93,7 +93,7 @@ class CustomFoodService {
         "protein": protein.toJson(),
         "totalFat": totalFat.toJson(),
         "saturatedFat": saturatedFat.toJson(),
-        "carbohydrate": carbohydrate.toJson(),
+        "carbohydrates": carbohydrate.toJson(),
         "sugars": sugars.toJson(),
         "fiber": fiber.toJson(),
         "sodium": sodium.toJson(),
